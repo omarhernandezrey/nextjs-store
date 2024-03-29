f9f8e157dd6d467d7b51e206f9be491d65937708
OmarHernandezRey <hernandezreyomar@gmail.com>
2024-03-29 11:14:19 -0500

    Uso de Sass en Next.jsClase 11 / 57

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,11 +1,11 @@
-import React from 'react'; // Add missing import for React
-import styles from './Hero.module.css';
+import styles from './Hero.module.sass'
 
 export const Hero = () => {
-    return(
-      <section className={styles.Hero}>
-        <h1>Future world</h1>
-        <h2>Empowering Your Tomorrow, Today!</h2>
-      </section>
-    )
+  
+  return (
+    <section className={styles.Hero}>
+      <h1>Future world</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
+    </section>
+  )
 }
