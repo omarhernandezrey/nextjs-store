e2f893b310052da993fa5368242c6b88d28116d1
OmarHernandezRey <hernandezreyomar@gmail.com>
2024-03-29 16:28:21 -0500

    Cómo agregar archivos estáticos en Next.jsClase 13 / 57

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,10 @@
-export const Description = () => {
-    return(
-      <section>
-        <h1>Descripción</h1>
-      </section>
-    )
-  }
+// Asegúrate de importar React si estás usando una versión de React antes de la 17 o si configuras tu proyecto para requerir importaciones explícitas.
+import React from 'react';
+
+export const Description = () => (
+  <section>
+    <img src="/images/description.jpeg" alt="Descripción del mercado de productos" />
+    <h2>Descripción</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorem, natus minus tenetur laudantium accusamus itaque quisquam voluptatibus odio nesciunt reiciendis illo magnam porro non voluptates. Ea consequatur eligendi at!</p>
+  </section>
+);
