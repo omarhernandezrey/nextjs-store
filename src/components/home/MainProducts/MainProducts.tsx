7af53c8e7790e724302f1f97d109a720a121f33d
OmarHernandezRey <hernandezreyomar@gmail.com>
2024-03-30 14:51:26 -0500

    Manejo de variables de entorno en Next.jsClase 19 / 57

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,11 @@
+"use client"
 export const MainProducts = () => {
-    return(
-      <section>
-        <h1>Main Products</h1>
-      </section>
-    )
-  }
+  // Correcto uso de variables de entorno en Next.js para el lado del cliente.
+  console.log("variable de entorno :", process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
+  
+  return (
+    <section>
+      <h1>Main Products</h1>
+    </section>
+  );
+};
