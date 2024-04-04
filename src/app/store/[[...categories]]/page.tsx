e6dc6ef43c8892e4440bf39094955515e2acab37
OmarHernandezRey <hernandezreyomar@gmail.com>
2024-04-04 10:33:53 -0500

    Arquitectura profesional para data fetching en un proyecto en Next.jsClase 27 / 57

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,15 +1,15 @@
 interface CategoryProps {
-    params: {
-      categories: string[],
-    }
-    searchParams?: string
+  params: {
+    categories: string[],
   }
-  
-  export default function Category(props: CategoryProps){
-    console.log(props)
-    const { categories } = props.params
-    console.log(categories)
-    return(
-      <h1>Categoria dinámica: {categories}</h1>
-    )
-  }
+  searchParams?: string
+}
+
+export default function Category(props: CategoryProps){
+
+  const { categories } = props.params
+  // throw new Error('Error: boom')
+  return(
+    <h1>Categoria dinámica: {categories}</h1>
+  )
+}
